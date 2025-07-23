f4065101526351375d69268f40aad816c4a2254d
George V. Kouryachy (Fr. Br. George) <george@altlinux.org>
2025-07-23 13:09:08 +0300

    Переписал сценарий по результатам обсуждения

--- 04_Debugging/Attached_materials/mkhasher.sh
+++ 04_Debugging/Attached_materials/mkhasher.sh
@@ -1,19 +1,18 @@
 #!/bin/sh -E
-PROG=mkhasher
-TEMP=$(getopt -o 'ip:rvw:h' --long 'init,rooter,packages:,verbose,workdir:,help' -n "$PROG" -- "$@") || exit $?
+PROG=`basename "$0"`
+TEMP=$(getopt -o 'p:rvw:h' --long 'rooter,packages:,verbose,workdir:,help' -n "$PROG" -- "$@") || exit $?
 eval set -- "$TEMP"
 unset TEMP
 
-ACTION="shell"
 WORKDIR="$HOME/hasher"
-ADDFLAGS=""
-MOUNTPOINTS=/proc
+SHFLAGS=""
+ALLFLAGS=""
+MOUNTPOINTS=/proc,/dev/pts
 ADDPACKAGES="vim-plugin-spec_alt-ftplugin vim-console tree rpm-utils"
 
 USAGE="$PROG — run hsh-shell with network on and some packages installed
 
 Usage: $PROG [OPTIONS] [SRC-RPM]
-    -i|--init     Initialize hasher directory before entering shell
     -p|--packages Additional packages to install
     -r|--rooter   Run shell commands as «rooter» user
     -w|--workdir  Specify hasher hierarchy directory ($WORKDIR)
@@ -21,28 +20,26 @@ Usage: $PROG [OPTIONS] [SRC-RPM]
     -h|--help	  Print this help
 
 SRC-RPM is source RPM file to build inside hasher.
+When installed, it's build dependencies will be installed as well.
 "
 while true; do
   case "$1" in
-    -i|--init) ACTION="init $ACTION";;
     -p|--packages) ADDPACKAGES="$ADDPACKAGES $2"; shift;;
-    -r|--rooter) ADDFLAGS="$ADDFLAGS --rooter";;
+    -r|--rooter) SHFLAGS="$SHFLAGS --rooter";;
     -w|--workdir) WORKDIR="$2"; shift;;
-    -v|--verbose) ADDFLAGS="$ADDFLAGS --verbose";;
+    -v|--verbose) ALLFLAGS="$ALLFLAGS --verbose";;
     -h|--help) echo "$USAGE" >&2; exit 0;;
     --) shift; break;;
   esac
   shift
 done
 
-test -z "$1" -a -z "$ADDFLAGS" && ACTION="shell"
-test -n "$1" && ACTION="build"
-for ACT in $ACTION; do
-  case $ACT in
-    build) hsh --workdir="$WORKDIR" --mountpoints=$MOUNTPOINTS $ADDFLAGS --lazy "$1"; exit $? ;;
-    init) hsh --workdir="$WORKDIR" --mountpoints=$MOUNTPOINTS $ADDFLGS --init;;
-    shell) hsh-copy --rooter /etc/resolv.conf /etc/resolv.conf
-           hsh-install --workdir="$WORKDIR" --mountpoints=$MOUNTPOINTS $ADDPACKAGES
-           share_network=1 hsh-shell $ADDFLAGS --workdir="$WORKDIR" --mountpoints=$MOUNTPOINTS;;
-  esac
-done
+test -d "$WORKDIR/chroot" || hsh --workdir="$WORKDIR" --mountpoints=$MOUNTPOINTS $ALLFLAGS --init
+if [ "$#" = 1 ]; then
+  hsh-rebuild --workdir="$WORKDIR" $ALLFLAGS --install-only "$1"
+  hsh-run --workdir="$WORKDIR" $ALLFLAGS rpm -i /usr/src/in/srpm/`basename "$1"`
+fi
+
+hsh-copy $ALLFLAGS --workdir="$WORKDIR" --rooter /etc/resolv.conf /etc/resolv.conf
+hsh-install $ALLFLAGS --workdir="$WORKDIR" --mountpoints=$MOUNTPOINTS $ADDPACKAGES
+share_network=1 hsh-shell $ALLFLAGS $SHFLAGS --workdir="$WORKDIR" --mountpoints=$MOUNTPOINTS
